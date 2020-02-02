9fd0d546c5357c515f10d1f3d97200e6d24a8a98
Vinicius Higuchi <vinicius-higuchi@outlook.com>
2020-02-02 01:40:08 -0300

    Initialization
    Added
    -Movement on click / mouse hold
    -Camera fixed follow
    Changes:
    -Terrain Tweaks

---
+++ Assets/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.position;
+    }
+}
